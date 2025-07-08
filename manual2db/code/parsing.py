0b03f29baf13cd93295f3bced4c784bbb04b877f
Marcus Dahlström <ma.dah@outlook.com>
2025-07-08 10:46:52 +0200

    Complete list of chemical elements

--- manual2db/code/parsing.py
+++ manual2db/code/parsing.py
@@ -918,6 +918,13 @@ def get_chemical_elements():
         'h', 'he', 'li', 'be', 'b', 'c', 'n', 'o', 'f', 'ne',
         'na', 'mg', 'al', 'si', 'p', 's', 'cl', 'ar', 'k', 'ca',
         'sc', 'ti', 'v', 'cr', 'mn', 'fe', 'co', 'ni', 'cu', 'zn',
-        'ga', 'ge', 'as', 'se', 'br', 'kr', 'rb', 'sr', 'y', 'zr']
+        'ga', 'ge', 'as', 'se', 'br', 'kr', 'rb', 'sr', 'y', 'zr',
+        'nb', 'mo', 'tc', 'ru', 'rh', 'pd', 'ag', 'cd', 'in', 'sn',
+        'sb', 'te', 'i', 'xe', 'cs', 'ba', 'la', 'ce', 'pr', 'nd',
+        'pm', 'sm', 'eu', 'gd', 'tb', 'dy', 'ho', 'er', 'tm', 'yb',
+        'lu', 'hf', 'ta', 'w', 're', 'os', 'ir', 'pt', 'au', 'hg',
+        'tl', 'pb', 'bi', 'po', 'at', 'rn', 'fr', 'ra', 'ac', 'th',
+        'pa', 'u'
+    ]
     
     return elements
